42e3222f4ca2ad8adeb321c32fcd590cbb6a97fb
Michal Chomo <michal.chomo@email.cz>
2017-09-26 18:51:07 +0300

    add getCode test for sequence of numbers with code size change

--- tests/gifTest.c
+++ tests/gifTest.c
@@ -13,7 +13,7 @@ void testGetCodeZeroes(CuTest *tc) {
     memset(buffer, 0, 9 * sizeof(uint8_t));
     bufferStart = buffer;
 
-    getCode(&buffer, 255);
+    getCode(NULL, 0xff);
 
     for (; codeSize <= LZW_MAX_CODE_SIZE; ++codeSize) {
         code = getCode(&buffer, codeSize);
@@ -37,7 +37,7 @@ void testGetCodeOnes(CuTest *tc) {
     memset(buffer, 0xff, 9 * sizeof(uint8_t));
     bufferStart = buffer;
 
-    getCode(&buffer, 255);
+    getCode(NULL, 0xff);
 
     for (; codeSize <= LZW_MAX_CODE_SIZE; ++codeSize) {
         code = getCode(&buffer, codeSize);
@@ -50,11 +50,51 @@ void testGetCodeOnes(CuTest *tc) {
     CuAssertIntEquals(tc, expectedSum, codeSum);
 }
 
+void testGetCodeSequence(CuTest *tc) {
+    uint8_t *buffer = NULL;
+    uint8_t *bufferStart = NULL;
+    uint8_t codeSize = 4;
+    uint16_t code = 0;
+    uint16_t expectedCodes[6] = {1, 2, 3, 20, 18, 29};
+    bool fail = false;
+
+    buffer = malloc(4 * sizeof(uint8_t));
+    memset(buffer, 0xff, 4 * sizeof(uint8_t));
+    bufferStart = buffer;
+
+    *buffer = 0x21;
+    ++buffer;
+    *buffer = 0x43;
+    ++buffer;
+    *buffer = 0x65;
+    ++buffer;
+    *buffer = 0x87;
+
+    buffer = bufferStart;
+
+    getCode(NULL, 0xff);
+
+    for (uint8_t i = 0; i < 6; ++i) {
+        if (i == 3) {
+            ++codeSize;
+        }
+        code = getCode(&buffer, codeSize);
+        if (code != expectedCodes[i]) {
+            fail = true;
+        }
+    }
+
+    free(bufferStart);
+
+    CuAssertTrue(tc, !fail);
+}
+
 CuSuite* GifGetSuite(void) {
     CuSuite* suite = CuSuiteNew();
 
     SUITE_ADD_TEST(suite, testGetCodeZeroes);
     SUITE_ADD_TEST(suite, testGetCodeOnes);
+    SUITE_ADD_TEST(suite, testGetCodeSequence);
 
     return suite;
 }
